d9f5bfff5bf4eabacb2073cfe5d10f3b506b307d
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-03-21 12:05:06 +0100

    ajout d'images + experiences

---
+++ experiences.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="experiences.css" />
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Mes experiences</title>
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+
+
+    <h2 class="experiences"><i class='bx bxs-briefcase-alt-2'></i>&nbsp;Mes experiences&nbsp;<i class='bx bxs-briefcase-alt-2'></i></h2>
+    <div class="stage1">
+        <h3>Développement d'un site internet de vente de voitures de location au Cameroun - Stage chez OpiumDigitale </h3><br>
+        <div class="logoStage">
+            <img src="img/opium digital.jfif" alt="opium digital">
+        </div>
+        <p>
+            Durant mon stage de 2 mois chez OpiumDigitale, j'ai eu l'opportunité de travailler sur le développement d'un site internet dédié à la vente de voitures de location au Cameroun. Mon rôle principal était de contribuer à la création de diverses fonctionnalités et à l'amélioration de l'interface utilisateur du site.<br>
+
+        <h3> Mes réalisations incluent :</h3><br>
+        Développement d'une page de filtre : J'ai conçu et implémenté une fonctionnalité permettant aux utilisateurs de filtrer les voitures selon leur prix et leur marque, améliorant ainsi l'expérience utilisateur et facilitant la recherche des véhicules.<br><br>
+        <div class="filtres">
+            <img src="img/filtre elitcar.png" alt="filtre-voiture">
+        </div>
+        Optimisation de la base de données : J'ai réorganisé la structure de la base de données afin de la rendre plus claire, plus lisible et plus efficace pour une gestion optimale des informations relatives aux véhicules.<br><br>
+        Création d'une page d'affichage de toutes les voitures : J'ai développé une page dédiée qui permet de lister l'ensemble des voitures disponibles, facilitant leur consultation par les utilisateurs.<br><br>
+        <div class="fiche">
+            <img src="img/fiche jeux elitcar.png" alt="fiche-voiture">
+        </div>
+        Développement d'une fiche détaillée pour chaque voiture : Chaque véhicule a sa propre page avec des informations spécifiques pour aider les utilisateurs à prendre une décision d'achat ou de location.<br><br>
+        Mise en place de l'ajout de photos : J'ai implémenté une fonctionnalité permettant d'ajouter des images pour chaque voiture, enrichissant ainsi les fiches produits et améliorant l'interaction visuelle avec les utilisateurs.<br><br>
+        Ce projet m'a permis de renforcer mes compétences en développement web et en gestion de bases de données, tout en contribuant activement à un projet concret qui répond à des besoins réels.
+        </p>
+    </div>
+    <div class="stage2">
+        <h3>Stage à l'INSTA (Jussieu, Paris) – 2 mois</h3><br>
+        <div class="logoInsta">
+        <img src="img/insta jussieu.jpg">
+        </div>
+        Lors de ce stage, j'ai travaillé sur plusieurs aspects du développement web :<br><br>
+
+        Sécurité : Implémentation d'un Captcha fonctionnel pour renforcer la protection contre les bots.<br><br>
+        <div class="captcha">
+        <img src="img/captcha.png">
+        </div>
+        Notifications par e-mail : Mise en place de l'envoi automatique d'e-mails aux utilisateurs lors d'un achat ou de la création d'un compte sur mon site e-commerce.<br><br>
+        <div class="email">
+        <img src="img/email.png">
+        </div>
+        Expérience utilisateur : Ajout d'une fonctionnalité de suggestion dynamique dans la barre de recherche, permettant d'afficher une liste de résultats en fonction des lettres saisies.<br><br>
+        <div class="data">
+        <img src="img/datalist.png">
+        </div>
+        Découverte de React : Début d'un projet en React afin d'acquérir de nouvelles compétences sur ce framework moderne.<br><br>
+        Ce stage m'a permis de renforcer mes compétences en développement web et d'explorer de nouvelles technologies.
+    </div>
+</body>
+
+</html>
+<?php
+include 'footer.php';
+?>
